38539b81d3753b6637658a5da91051242af42c55
vanere27 <vrestrepoo@unal.edu.co>
2025-11-13 17:29:34 -0500

    sistemasRQ

--- sistemaRQ/routes/api.php
+++ sistemaRQ/routes/api.php
@@ -3,5 +3,4 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-Route::middleware([]) 'auth:sanctum'], function () {
-    Route::post('/logout', [UserController::class,'logout']);
+Route::middleware(['auth:sanctum'])->get('/personas', [PersonaController::class, 'generar']);
